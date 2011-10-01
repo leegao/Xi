d6b2574a15d8e74173551d474642a6ed8b8f019f
Dan Chen <danchen@rrdhcp-60-183.redrover.cornell.edu>
2011-10-01 15:21:09 -0400

    type checking implementation

--- src/cs4120/der34dlc287lg342/xi/ast/ProcedureCallNode.java
+++ src/cs4120/der34dlc287lg342/xi/ast/ProcedureCallNode.java
@@ -1,9 +1,15 @@
 package cs4120.der34dlc287lg342.xi.ast;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import cs4120.der34dlc287lg342.xi.typechecker.XiPrimitiveType;
+import cs4120.der34dlc287lg342.xi.typechecker.XiType;
+import cs4120.der34dlc287lg342.xi.typechecker.XiTypeContext;
 
 import edu.cornell.cs.cs4120.util.VisualizableTreeNode;
 import edu.cornell.cs.cs4120.xi.AbstractSyntaxNode;
+import edu.cornell.cs.cs4120.xi.CompilationException;
 import edu.cornell.cs.cs4120.xi.Position;
 
 public class ProcedureCallNode extends AbstractSyntaxTree {
@@ -28,6 +34,13 @@ public class ProcedureCallNode extends AbstractSyntaxTree {
 	@Override
 	public String label() {
 		return "PROCEDURE";
+	}
+	
+	@Override
+	public XiType typecheck(List<XiTypeContext> stack) throws CompilationException {
+		XiType callType = ((AbstractSyntaxTree)call).typecheck(stack);
+		if(callType.equals(XiPrimitiveType.UNIT)) return XiPrimitiveType.UNIT;
+		throw new CompilationException("Function has return type, expecting no return type", position());
 	}
 
 }
