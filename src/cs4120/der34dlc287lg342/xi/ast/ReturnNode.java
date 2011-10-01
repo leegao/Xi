8fb684670acb1b928d6e57eec9ba5c43a14c419a
Dan Chen <danchen@rrdhcp-60-183.redrover.cornell.edu>
2011-10-01 16:08:22 -0400

    adding implementation of typecheck()

--- src/cs4120/der34dlc287lg342/xi/ast/ReturnNode.java
+++ src/cs4120/der34dlc287lg342/xi/ast/ReturnNode.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import cs4120.der34dlc287lg342.xi.typechecker.ContextList;
 import cs4120.der34dlc287lg342.xi.typechecker.XiFunctionType;
+import cs4120.der34dlc287lg342.xi.typechecker.XiPrimitiveType;
 import cs4120.der34dlc287lg342.xi.typechecker.XiType;
+import cs4120.der34dlc287lg342.xi.typechecker.InvalidXiTypeException;
 
 import edu.cornell.cs.cs4120.util.VisualizableTreeNode;
 import edu.cornell.cs.cs4120.xi.AbstractSyntaxNode;
@@ -37,15 +39,22 @@ public class ReturnNode extends AbstractSyntaxTree {
 	
 	@Override
 	public XiType typecheck(ContextList stack) throws CompilationException {
-		XiFunctionType retTypes = stack.find_return();
+		XiFunctionType function;
+		try {
+			function = stack.find_return();
+		}catch (InvalidXiTypeException ex) {
+			throw new CompilationException(ex.getMessage(),position);
+		}
 		
-		if (retTypes != null) {
-			if (retTypes.ret.size() == children.size())
+		if (function != null) {
+			if (function.ret.size() == children.size())
 			{
-				for (int index = 0; index < retTypes.ret.size(); index++) {
-					if (!((AbstractSyntaxTree)children.get(index)).typecheck((stack).equals(retTypes.ret.get(index)))
+				for (int index = 0; index < function.ret.size(); index++) {
+					if (!((AbstractSyntaxTree)children.get(index)).typecheck(stack).equals(function.ret.get(index)))
 						throw new CompilationException("Invalid return type at index " + index, position);
 				}
+				
+				return XiPrimitiveType.VOID;
 			} else {
 				throw new CompilationException("Invalid number of return types", position);
 			}
--- src/cs4120/der34dlc287lg342/xi/typechecker/XiPrimitiveType.java
+++ src/cs4120/der34dlc287lg342/xi/typechecker/XiPrimitiveType.java
@@ -50,6 +50,7 @@ public class XiPrimitiveType implements XiType {
 	public static XiType UNIT = new XiPrimitiveType("unit");
 	public static XiType INT = new XiPrimitiveType("int");
 	public static XiType BOOL = new XiPrimitiveType("bool");
+	public static XiType VOID = new XiPrimitiveType("void");
 	
 	public static XiPrimitiveType array(XiPrimitiveType t){
 		XiPrimitiveType nt = (XiPrimitiveType)t.clone();
