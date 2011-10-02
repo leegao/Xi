b71b1a043f5c33f0fffaac6db4bff62daed5a991
Lee Gao <leeyuan123@yahoo.com>
2011-10-01 23:12:58 -0400

    Added typechecking for breaks

--- src/cs4120/der34dlc287lg342/xi/ast/BreakNode.java
+++ src/cs4120/der34dlc287lg342/xi/ast/BreakNode.java
@@ -2,8 +2,15 @@ package cs4120.der34dlc287lg342.xi.ast;
 
 import java.util.ArrayList;
 
+import cs4120.der34dlc287lg342.xi.typechecker.ContextList;
+import cs4120.der34dlc287lg342.xi.typechecker.InvalidXiTypeException;
+import cs4120.der34dlc287lg342.xi.typechecker.XiFunctionType;
+import cs4120.der34dlc287lg342.xi.typechecker.XiPrimitiveType;
+import cs4120.der34dlc287lg342.xi.typechecker.XiType;
+
 import edu.cornell.cs.cs4120.util.VisualizableTreeNode;
 import edu.cornell.cs.cs4120.xi.AbstractSyntaxNode;
+import edu.cornell.cs.cs4120.xi.CompilationException;
 import edu.cornell.cs.cs4120.xi.Position;
 
 public class BreakNode extends AbstractSyntaxTree {
@@ -29,4 +36,20 @@ public class BreakNode extends AbstractSyntaxTree {
 		return "BREAK";
 	}
 
+	@Override
+	public XiType typecheck(ContextList stack) throws CompilationException {
+		boolean breakable;
+		try {
+			breakable = stack.breakable();
+		}catch (InvalidXiTypeException ex) {
+			throw new CompilationException(ex.getMessage(),position);
+		}
+		
+		if (!breakable){
+			throw new CompilationException("Cannot break if not in a loop", position);
+		}
+		
+		type = XiPrimitiveType.UNIT;
+		return type;
+	}
 }
