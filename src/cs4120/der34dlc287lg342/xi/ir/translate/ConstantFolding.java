51412fd0473367fb5dd6320e044d12995acf1fa2
Lee Gao <leeyuan123@yahoo.com>
2011-10-18 13:51:13 -0400

    Added in constant folding class and testing to see if new setup works

---
+++ src/cs4120/der34dlc287lg342/xi/ir/translate/ConstantFolding.java
@@ -0,0 +1,10 @@
+package cs4120.der34dlc287lg342.xi.ir.translate;
+
+import cs4120.der34dlc287lg342.xi.ir.Seq;
+
+public class ConstantFolding {
+	public static Seq foldConstants(Seq stmts){
+		
+		return null;
+	}
+}
