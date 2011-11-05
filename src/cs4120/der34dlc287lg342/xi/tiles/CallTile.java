388bc4aa8a31fbc6330a92239dc6832cce5de948
Lee Gao <leeyuan123@yahoo.com>
2011-11-05 19:16:12 -0400

    Added the assembly code for call tiles

--- src/cs4120/der34dlc287lg342/xi/tiles/CallTile.java
+++ src/cs4120/der34dlc287lg342/xi/tiles/CallTile.java
@@ -7,6 +7,7 @@ import cs4120.der34dlc287lg342.xi.ir.context.Label;
 public class CallTile extends Tile{
 	public Label name;
 	public ArrayList<Tile> args;
+	public static String[] registers = {"", ""};
 	public CallTile(Label name){
 		this.name = name;
 		args = new ArrayList<Tile>();
@@ -14,5 +15,20 @@ public class CallTile extends Tile{
 	
 	public void add(Tile tile){
 		args.add(tile);
+	}
+	
+	public String att(){
+		String asm = "";
+		int i = 0;
+		for (Tile arg : args){
+			asm += arg.att();
+			if (i < 6){
+				asm += "movq "+arg.out+", "+registers[i++]+"\n";
+			} else {
+				asm += "pushq "+arg.out+"\n";
+			}
+		}
+		asm += "call "+name;
+		return asm;
 	}
 }
