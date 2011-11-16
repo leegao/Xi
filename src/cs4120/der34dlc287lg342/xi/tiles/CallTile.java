0a0b1675b5a86a978dde52fecee57159a2b28027
Lee <leeyuan123@yahoo.com>
2011-11-15 23:25:57 -0500

    Fixed att of CallTile

--- src/cs4120/der34dlc287lg342/xi/tiles/CallTile.java
+++ src/cs4120/der34dlc287lg342/xi/tiles/CallTile.java
@@ -3,6 +3,7 @@ package cs4120.der34dlc287lg342.xi.tiles;
 import java.util.ArrayList;
 
 import cs4120.der34dlc287lg342.xi.assembly.Assembly;
+import cs4120.der34dlc287lg342.xi.assembly.OPER;
 import cs4120.der34dlc287lg342.xi.ir.context.Label;
 import cs4120.der34dlc287lg342.xi.ir.context.TempRegister;
 
@@ -21,29 +22,61 @@ public class CallTile extends Tile{
 		args.add(tile);
 	}
 	
+//	public ArrayList<Assembly> att(){
+//		String asm = "";
+//		int i = 0;
+//		if (tuple != null){
+//			asm += "movq " + tuple + ", %rdi\n";
+//			
+//			i++;
+//		}
+//		for (Tile arg : args){
+//			asm += arg.att();
+//			if (i < 6){
+//				if (name == Label.internal_strdup)
+//					asm += "leaq "+ arg + ", %"+TempRegister.free_registers[i++].name+"\n";
+//				else
+//					asm += "movq "+ arg.out+", %"+TempRegister.free_registers[i++].name+"\n";
+//			} else {
+//				asm += "pushq "+arg.out+"\n";
+//			}
+//		}
+//		asm += "call "+name+"\n";
+//		if (args.size() > 6)
+//			for (int j = 0; j < args.size()-6; j++)
+//				asm += "popq %r15\n";
+//		out = TempRegister.RV;
+//		return asm;
+//	}
+	
 	public ArrayList<Assembly> att(){
-		String asm = "";
+		ArrayList<Assembly> asm = new ArrayList<Assembly>();
 		int i = 0;
 		if (tuple != null){
-			asm += "movq " + tuple + ", %rdi\n";
-			
+			//asm += "movq " + tuple + ", %rdi\n";
+			asm.add(new OPER("movq %s0, %rdi", tuple, null));
 			i++;
 		}
 		for (Tile arg : args){
-			asm += arg.att();
+			asm.addAll(arg.att());
+			
 			if (i < 6){
 				if (name == Label.internal_strdup)
-					asm += "leaq "+ arg + ", %"+TempRegister.free_registers[i++].name+"\n";
+					//asm += "leaq "+ arg + ", %"+TempRegister.free_registers[i++].name+"\n";
+					asm.add(new OPER("leaq %s0, %"+TempRegister.free_registers[i++].name, arg.out, null));
 				else
-					asm += "movq "+ arg.out+", %"+TempRegister.free_registers[i++].name+"\n";
+					//asm += "movq "+ arg.out+", %"+TempRegister.free_registers[i++].name+"\n";
+					asm.add(new OPER("movq %s0, %"+TempRegister.free_registers[i++].name, arg.out, null));
 			} else {
-				asm += "pushq "+arg.out+"\n";
+				//asm += "pushq "+arg.out+"\n";
+				asm.add(new OPER("pushq %s0", arg.out, null));
 			}
 		}
-		asm += "call "+name+"\n";
+		asm.add(new OPER("call "+name, new TempRegister[]{}, null));
 		if (args.size() > 6)
 			for (int j = 0; j < args.size()-6; j++)
-				asm += "popq %r15\n";
+				//asm += "popq %rdx\n";
+				asm.add(new OPER("popq %rdx", new TempRegister[]{}, null));
 		out = TempRegister.RV;
 		return asm;
 	}
